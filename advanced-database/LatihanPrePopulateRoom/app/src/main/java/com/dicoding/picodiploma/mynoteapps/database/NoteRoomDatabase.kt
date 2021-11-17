e1c7c87993dd167ac95ab679526f2dacb63a358b
Ahmad Arif Faizin <faizin.arief@gmail.com>
2021-11-17 09:18:25 +0700

    Add prepopulate

--- advanced-database/LatihanPrePopulateRoom/app/src/main/java/com/dicoding/picodiploma/mynoteapps/database/NoteRoomDatabase.kt
+++ advanced-database/LatihanPrePopulateRoom/app/src/main/java/com/dicoding/picodiploma/mynoteapps/database/NoteRoomDatabase.kt
@@ -4,6 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.dicoding.picodiploma.mynoteapps.R
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.concurrent.Executors
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteRoomDatabase : RoomDatabase() {
@@ -20,10 +29,61 @@ abstract class NoteRoomDatabase : RoomDatabase() {
                 synchronized(NoteRoomDatabase::class.java) {
                     INSTANCE = Room.databaseBuilder(context.applicationContext,
                             NoteRoomDatabase::class.java, "note_database")
-                            .build()
+                        .addCallback(object : Callback() {
+                            override fun onCreate(db: SupportSQLiteDatabase) {
+                                super.onCreate(db)
+                                INSTANCE?.let { database ->
+                                    Executors.newSingleThreadScheduledExecutor().execute {
+                                        fillWithStartingData(context.applicationContext, database.noteDao())
+                                    }
+                                }
+
+                            }
+                        })
+                        .build()
                 }
             }
             return INSTANCE as NoteRoomDatabase
+        }
+
+        private fun fillWithStartingData(context: Context, dao: NoteDao) {
+            val task = loadJsonArray(context)
+            try {
+                if (task != null) {
+                    for (i in 0 until task.length()) {
+                        val item = task.getJSONObject(i)
+                        dao.insert(
+                            Note(
+                                item.getInt("id"),
+                                item.getString("title"),
+                                item.getString("description"),
+                                item.getString("date")
+                            )
+                        )
+                    }
+                }
+            } catch (exception: JSONException) {
+                exception.printStackTrace()
+            }
+        }
+
+        private fun loadJsonArray(context: Context): JSONArray? {
+            val builder = StringBuilder()
+            val `in` = context.resources.openRawResource(R.raw.note)
+            val reader = BufferedReader(InputStreamReader(`in`))
+            var line: String?
+            try {
+                while (reader.readLine().also { line = it } != null) {
+                    builder.append(line)
+                }
+                val json = JSONObject(builder.toString())
+                return json.getJSONArray("notes")
+            } catch (exception: IOException) {
+                exception.printStackTrace()
+            } catch (exception: JSONException) {
+                exception.printStackTrace()
+            }
+            return null
         }
     }
 }

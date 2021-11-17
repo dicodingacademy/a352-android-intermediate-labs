942f01419498f0de40d2e3078132bf5e7fafe34f
Ahmad Arif Faizin <faizin.arief@gmail.com>
2021-11-17 09:22:56 +0700

    Add initial relation room (with prepopulate)

---
+++ advanced-database/LatihanRelasiRoom/app/src/main/java/com/dicoding/picodiploma/mynoteapps/ui/main/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.dicoding.picodiploma.mynoteapps.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.picodiploma.mynoteapps.R
+import com.dicoding.picodiploma.mynoteapps.databinding.ActivityMainBinding
+import com.dicoding.picodiploma.mynoteapps.helper.ViewModelFactory
+import com.dicoding.picodiploma.mynoteapps.ui.insert.NoteAddUpdateActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+
+    private lateinit var adapter: NoteAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this, { noteList ->
+            if (noteList != null) {
+                adapter.setListNotes(noteList)
+            }
+        })
+
+        adapter = NoteAdapter()
+
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+
+        binding?.fabAdd?.setOnClickListener { view ->
+            if (view.id == R.id.fab_add) {
+                val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
+    }
+
+}
---
+++ advanced-database/LatihanRelasiRoom/app/src/main/java/com/dicoding/picodiploma/mynoteapps/ui/main/NoteAdapter.kt
@@ -0,0 +1,52 @@
+package com.dicoding.picodiploma.mynoteapps.ui.main
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.picodiploma.mynoteapps.database.Note
+import com.dicoding.picodiploma.mynoteapps.databinding.ItemNoteBinding
+import com.dicoding.picodiploma.mynoteapps.helper.NoteDiffCallback
+import com.dicoding.picodiploma.mynoteapps.ui.insert.NoteAddUpdateActivity
+import com.dicoding.picodiploma.mynoteapps.ui.main.NoteAdapter.NoteViewHolder
+import java.util.*
+
+class NoteAdapter : RecyclerView.Adapter<NoteViewHolder>() {
+    private val listNotes = ArrayList<Note>()
+    fun setListNotes(listNotes: List<Note>) {
+        val diffCallback = NoteDiffCallback(this.listNotes, listNotes)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+        this.listNotes.clear()
+        this.listNotes.addAll(listNotes)
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = ItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(listNotes[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listNotes.size
+    }
+
+    inner class NoteViewHolder(private val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: Note) {
+            with(binding) {
+                tvItemTitle.text = note.title
+                tvItemDate.text = note.date
+                tvItemDescription.text = note.description
+                cvItemNote.setOnClickListener {
+                    val intent = Intent(it.context, NoteAddUpdateActivity::class.java)
+                    intent.putExtra(NoteAddUpdateActivity.EXTRA_NOTE, note)
+                    it.context.startActivity(intent)
+                }
+            }
+        }
+    }
+}

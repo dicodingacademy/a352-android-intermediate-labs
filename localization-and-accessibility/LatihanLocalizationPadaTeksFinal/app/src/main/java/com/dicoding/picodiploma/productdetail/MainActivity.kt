dee9823f78444f2daff0f4c0722406fdec0906a6
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2021-12-13 14:30:29 +0700

    Add Final Project Localiztaion

---
+++ localization-and-accessibility/LatihanLocalizationPadaTeksFinal/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.dicoding.picodiploma.productdetail
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}

5ef5cba3a4c3c9b3c56cf862a2cd52af290a5ca6
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2021-12-16 15:08:52 +0700

    Update Final Project Localization

--- localization-and-accessibility/LatihanLocalizationPadaTeksFinal/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
+++ localization-and-accessibility/LatihanLocalizationPadaTeksFinal/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.dicoding.picodiploma.productdetail
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.WindowInsets
 import android.view.WindowManager
+import com.dicoding.picodiploma.productdetail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setupView()
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.settingImageView.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
     }
 
     private fun setupView() {

9ff62d283b7f30a68730a25e7bb625d206f9f689
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-01-03 15:07:27 +0700

    Starter Project for SharedElement

---
+++ animation/LatihanSharedElementStarter/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/HeroActivity.kt
@@ -0,0 +1,27 @@
+package com.dicoding.picodiploma.myrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.dicoding.picodiploma.myrecyclerview.model.Hero
+
+class HeroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hero)
+
+        setupData()
+    }
+
+    private fun setupData() {
+        val hero = intent.getParcelableExtra<Hero>("Hero") as Hero
+        Glide.with(applicationContext)
+            .load(hero.photo)
+            .circleCrop()
+            .into(findViewById(R.id.profileImageView))
+        findViewById<TextView>(R.id.nameTextView).text = hero.name
+        findViewById<TextView>(R.id.descTextView).text = hero.description
+    }
+}
---
+++ animation/LatihanSharedElementStarter/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/model/Hero.kt
@@ -0,0 +1,17 @@
+package com.dicoding.picodiploma.myrecyclerview.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
+
+
+/**
+ * Created by sidiqpermana on 10/29/16.
+ */
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable

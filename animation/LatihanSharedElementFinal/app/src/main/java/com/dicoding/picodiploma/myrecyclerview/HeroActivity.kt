efecc8a22fc8ee79bd5cb157ea56261201a4b3df
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-01-05 17:02:12 +0700

    update shared element project

--- animation/LatihanSharedElementFinal/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/HeroActivity.kt
+++ animation/LatihanSharedElementFinal/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/HeroActivity.kt
@@ -11,6 +11,7 @@ class HeroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hero)
+
         setupData()
     }
 
---
+++ animation/LatihanSharedElementFinal/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/adapter/ListHeroAdapter.kt
@@ -0,0 +1,64 @@
+package com.dicoding.picodiploma.myrecyclerview.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dicoding.picodiploma.myrecyclerview.HeroActivity
+import com.dicoding.picodiploma.myrecyclerview.R
+import com.dicoding.picodiploma.myrecyclerview.model.Hero
+import android.app.Activity
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+
+/**
+ * Created by sidiqpermana on 10/28/16.
+ */
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listHero[position])
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var imgPhoto: ImageView = itemView.findViewById(R.id.profileImageView)
+        private var tvName: TextView = itemView.findViewById(R.id.nameTextView)
+        private var tvDescription: TextView = itemView.findViewById(R.id.descTextView)
+
+        fun bind(hero: Hero) {
+            Glide.with(itemView.context)
+                .load(hero.photo)
+                .circleCrop()
+                .into(imgPhoto)
+            tvName.text = hero.name
+            tvDescription.text = hero.description
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, HeroActivity::class.java)
+                intent.putExtra("Hero", hero)
+
+                val optionsCompat: ActivityOptionsCompat =
+                    ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        itemView.context as Activity,
+                        Pair(imgPhoto, "profile"),
+                        Pair(tvName, "name"),
+                        Pair(tvDescription, "description"),
+                    )
+                itemView.context.startActivity(intent, optionsCompat.toBundle())
+            }
+        }
+    }
+
+}

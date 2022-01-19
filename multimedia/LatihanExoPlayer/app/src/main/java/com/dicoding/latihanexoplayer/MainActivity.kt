475666f177cbd63baa360fd0a674e8d3f1d2ad35
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-01-19 15:11:39 +0700

    Update Project

--- multimedia/LatihanExoPlayer/app/src/main/java/com/dicoding/latihanexoplayer/MainActivity.kt
+++ multimedia/LatihanExoPlayer/app/src/main/java/com/dicoding/latihanexoplayer/MainActivity.kt
@@ -11,72 +11,70 @@ import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.util.Util
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val URL_VIDEO_DICODING = "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+    }
+
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private var player: ExoPlayer? = null
-
-    private var playWhenReady = true
-    private var currentWindow = 0
-    private var playbackPosition = 0L
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+
+        val player = ExoPlayer.Builder(this).build()
+        viewBinding.videoView.player = player
+
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        player.setMediaItem(mediaItem)
+        player.prepare()
     }
 
-    public override fun onStart() {
-        super.onStart()
-        if (Util.SDK_INT > 23) {
-            initializePlayer()
-        }
-    }
-
-    public override fun onResume() {
-        super.onResume()
-        if (Util.SDK_INT <= 23 || player == null) {
-            initializePlayer()
-        }
-    }
-
-    public override fun onPause() {
-        super.onPause()
-        if (Util.SDK_INT <= 23) {
-            releasePlayer()
-        }
-    }
-
-    public override fun onStop() {
-        super.onStop()
-        if (Util.SDK_INT > 23) {
-            releasePlayer()
-        }
-    }
-
-    private fun initializePlayer() {
-        player = ExoPlayer.Builder(this)
-            .build()
-            .also { exoPlayer ->
-                viewBinding.videoView.player = exoPlayer
-
-                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
-                exoPlayer.setMediaItem(mediaItem)
-                val secondMediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
-                exoPlayer.addMediaItem(secondMediaItem)
-                exoPlayer.playWhenReady = playWhenReady
-                exoPlayer.seekTo(currentWindow, playbackPosition)
-                exoPlayer.prepare()
-            }
-    }
-
-    private fun releasePlayer() {
-        player?.run {
-            playbackPosition = this.currentPosition
-            currentWindow = this.currentMediaItemIndex
-            playWhenReady = this.playWhenReady
-            release()
-        }
-        player = null
-    }
+//    public override fun onStart() {
+//        super.onStart()
+//        if (Util.SDK_INT > 23) {
+//            initializePlayer()
+//        }
+//    }
+//
+//    public override fun onResume() {
+//        super.onResume()
+//        if (Util.SDK_INT <= 23 || player == null) {
+//            initializePlayer()
+//        }
+//    }
+//
+//    public override fun onPause() {
+//        super.onPause()
+//        if (Util.SDK_INT <= 23) {
+//            releasePlayer()
+//        }
+//    }
+//
+//    public override fun onStop() {
+//        super.onStop()
+//        if (Util.SDK_INT > 23) {
+//            releasePlayer()
+//        }
+//    }
+//
+//    private fun initializePlayer() {
+//        player = ExoPlayer.Builder(this)
+//            .build()
+//            .also { exoPlayer ->
+//                viewBinding.videoView.player = exoPlayer
+//
+//                val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+//                exoPlayer.setMediaItem(mediaItem)
+////                exoPlayer.playWhenReady = false
+////                exoPlayer.seekTo(currentWindow, playbackPosition)
+//                exoPlayer.prepare()
+//            }
+//    }
+//
+//    private fun releasePlayer() {
+//        player?.release()
+//        player = null
+//    }
 }

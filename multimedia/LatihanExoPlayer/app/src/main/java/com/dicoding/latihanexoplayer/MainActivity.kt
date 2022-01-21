bd7eba18914de65c030c4e9744870c8fe2803efa
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-01-21 10:47:26 +0700

    Update MainActivity.kt

--- multimedia/LatihanExoPlayer/app/src/main/java/com/dicoding/latihanexoplayer/MainActivity.kt
+++ multimedia/LatihanExoPlayer/app/src/main/java/com/dicoding/latihanexoplayer/MainActivity.kt
@@ -12,7 +12,10 @@ import com.google.android.exoplayer2.util.Util
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        const val URL_VIDEO_DICODING = "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+        const val URL_VIDEO_DICODING =
+            "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+        const val URL_AUDIO =
+            "https://cdn.pixabay.com/download/audio/2022/01/18/audio_d0a13f69d2.mp3?filename=indie-folk-king-around-here-15045.mp3"
     }
 
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
@@ -22,59 +25,62 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
-
-        val player = ExoPlayer.Builder(this).build()
-        viewBinding.videoView.player = player
-
-        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
-        player.setMediaItem(mediaItem)
-        player.prepare()
     }
 
-//    public override fun onStart() {
-//        super.onStart()
-//        if (Util.SDK_INT > 23) {
-//            initializePlayer()
-//        }
-//    }
-//
-//    public override fun onResume() {
-//        super.onResume()
-//        if (Util.SDK_INT <= 23 || player == null) {
-//            initializePlayer()
-//        }
-//    }
-//
-//    public override fun onPause() {
-//        super.onPause()
-//        if (Util.SDK_INT <= 23) {
-//            releasePlayer()
-//        }
-//    }
-//
-//    public override fun onStop() {
-//        super.onStop()
-//        if (Util.SDK_INT > 23) {
-//            releasePlayer()
-//        }
-//    }
-//
-//    private fun initializePlayer() {
-//        player = ExoPlayer.Builder(this)
-//            .build()
-//            .also { exoPlayer ->
-//                viewBinding.videoView.player = exoPlayer
-//
-//                val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
-//                exoPlayer.setMediaItem(mediaItem)
-////                exoPlayer.playWhenReady = false
-////                exoPlayer.seekTo(currentWindow, playbackPosition)
-//                exoPlayer.prepare()
-//            }
-//    }
-//
-//    private fun releasePlayer() {
-//        player?.release()
-//        player = null
-//    }
+    private var player: ExoPlayer? = null
+
+    private fun initializePlayer() {
+        val mediaItem = MediaItem.fromUri(MainActivity.URL_VIDEO_DICODING)
+        val anotherMediaItem = MediaItem.fromUri(MainActivity.URL_AUDIO)
+
+        player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+            viewBinding.videoView.player = exoPlayer
+            exoPlayer.setMediaItem(mediaItem)
+            exoPlayer.addMediaItem(anotherMediaItem)
+            exoPlayer.prepare()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.release()
+        player = null
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        if (Util.SDK_INT <= 23 && player == null) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
 }

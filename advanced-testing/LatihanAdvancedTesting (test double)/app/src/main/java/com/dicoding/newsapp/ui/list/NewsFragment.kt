d5701bf69b6e53c83eae99a14b17c156ba1fa593
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-01-28 11:42:28 +0700

    add livedata test

--- advanced-testing/LatihanAdvancedTesting (test double)/app/src/main/java/com/dicoding/newsapp/ui/list/NewsFragment.kt
+++ advanced-testing/LatihanAdvancedTesting (test double)/app/src/main/java/com/dicoding/newsapp/ui/list/NewsFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
--- advanced-testing/LatihanAdvancedTesting (test double)/app/src/main/java/com/dicoding/newsapp/ui/list/NewsViewModel.kt
+++ advanced-testing/LatihanAdvancedTesting (test double)/app/src/main/java/com/dicoding/newsapp/ui/list/NewsViewModel.kt
@@ -1,10 +1,7 @@
 package com.dicoding.newsapp.ui.list
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.dicoding.newsapp.data.NewsRepository
-import com.dicoding.newsapp.data.local.entity.NewsEntity
-import kotlinx.coroutines.launch
 
 class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
     fun getHeadlineNews() = newsRepository.getHeadlineNews()

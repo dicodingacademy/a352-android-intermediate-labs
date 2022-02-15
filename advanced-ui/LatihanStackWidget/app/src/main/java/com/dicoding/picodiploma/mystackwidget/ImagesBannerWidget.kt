19416cc83e6466326424e0949effc3a066f6919d
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2022-02-16 04:10:11 +0700

    Bug Fix and update build gradle on Stack Widget

--- advanced-ui/LatihanStackWidget/app/src/main/java/com/dicoding/picodiploma/mystackwidget/ImagesBannerWidget.kt
+++ advanced-ui/LatihanStackWidget/app/src/main/java/com/dicoding/picodiploma/mystackwidget/ImagesBannerWidget.kt
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
 import android.widget.Toast
 import androidx.core.net.toUri
@@ -34,8 +35,12 @@ class ImagesBannerWidget : AppWidgetProvider() {
             val toastIntent = Intent(context, ImagesBannerWidget::class.java)
             toastIntent.action = TOAST_ACTION
             toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
-            val toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+                else 0
+            )
             views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
 
             appWidgetManager.updateAppWidget(appWidgetId, views)

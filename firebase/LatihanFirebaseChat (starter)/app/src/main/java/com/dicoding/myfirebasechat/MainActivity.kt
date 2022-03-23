b46f74dcba11a2e9a8118c7a0f9e38e732927456
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-03-23 10:19:19 +0700

    add starter project

---
+++ firebase/LatihanFirebaseChat (starter)/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.dicoding.myfirebasechat
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.myfirebasechat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.sign_out_menu -> {
+                signOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun signOut() {
+
+    }
+}

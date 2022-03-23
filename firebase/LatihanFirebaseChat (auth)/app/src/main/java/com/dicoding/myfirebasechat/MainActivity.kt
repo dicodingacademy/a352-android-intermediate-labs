b9c15e667a5d7f42fae9f89df8ecc56da9438b1e
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-03-23 17:31:35 +0700

    add auth

--- firebase/LatihanFirebaseChat (auth)/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
+++ firebase/LatihanFirebaseChat (auth)/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.dicoding.myfirebasechat
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.myfirebasechat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            // Not signed in, launch the Login activity
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            return
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -34,6 +46,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun signOut() {
-
+        auth.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }

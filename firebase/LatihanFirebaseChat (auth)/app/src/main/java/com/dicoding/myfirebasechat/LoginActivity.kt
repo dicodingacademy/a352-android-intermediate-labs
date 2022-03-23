ef307751d3fdb8bcb109b76fc978096a5d7bcad9
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-03-23 16:05:19 +0700

    add auth starter

---
+++ firebase/LatihanFirebaseChat (auth)/app/src/main/java/com/dicoding/myfirebasechat/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.dicoding.myfirebasechat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}

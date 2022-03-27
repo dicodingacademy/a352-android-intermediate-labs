bcae26159290496343769b347e513b9679da2c24
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-03-27 19:07:21 +0700

    add fcm

---
+++ firebase/LatihanFirebaseChat (fcm)/app/src/main/java/com/dicoding/myfirebasechat/MyFirebaseMessagingService.kt
@@ -0,0 +1,61 @@
+package com.dicoding.myfirebasechat
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage.from}")
+        Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        Log.d(TAG, "Message Notification Body: ${remoteMessage.notification?.body}")
+
+        sendNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
+    }
+
+    private fun sendNotification(title: String?, messageBody: String?) {
+        val contentIntent = Intent(applicationContext, MainActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notificationBuilder = NotificationCompat.Builder(applicationContext,
+            NOTIFICATION_CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setContentIntent(contentPendingIntent)
+            .setAutoCancel(true)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            notificationBuilder.setChannelId(NOTIFICATION_CHANNEL_ID)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    companion object {
+        private val TAG = MyFirebaseMessagingService::class.java.simpleName
+        private const val NOTIFICATION_ID = 1
+        private const val NOTIFICATION_CHANNEL_ID = "Firebase Channel"
+        private const val NOTIFICATION_CHANNEL_NAME = "Firebase Notification"
+    }
+}

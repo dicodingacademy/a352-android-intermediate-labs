f1a334f98fa1ee144c356984c8cd974e877e7a2d
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-04-01 10:05:52 +0700

    update code

--- media/LatihanCameraXStarter/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ media/LatihanCameraXStarter/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -13,18 +13,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.cameraXButton.setOnClickListener {
-            Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
-        }
-        binding.cameraButton.setOnClickListener {
-            Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
-        }
-        binding.galleryButton.setOnClickListener {
-            Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
-        }
-        binding.uploadButton.setOnClickListener {
-            Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
-        }
+        binding.cameraXButton.setOnClickListener { startCameraX() }
+        binding.cameraButton.setOnClickListener { startTakePhoto() }
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.uploadButton.setOnClickListener { uploadImage() }
+    }
+
+    private fun uploadImage() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startGallery() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startTakePhoto() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startCameraX() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
     }
 
 }

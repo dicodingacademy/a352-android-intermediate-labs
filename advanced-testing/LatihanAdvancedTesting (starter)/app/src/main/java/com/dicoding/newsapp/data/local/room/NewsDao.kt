919e9c93e706b7432b2b03aca06d9139d6385d86
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-05-30 09:36:38 +0700

    Update gradle

--- advanced-testing/LatihanAdvancedTesting (starter)/app/src/main/java/com/dicoding/newsapp/data/local/room/NewsDao.kt
+++ advanced-testing/LatihanAdvancedTesting (starter)/app/src/main/java/com/dicoding/newsapp/data/local/room/NewsDao.kt
@@ -1,16 +1,16 @@
 package com.dicoding.newsapp.data.local.room
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.dicoding.newsapp.data.local.entity.NewsEntity
 
 @Dao
 interface NewsDao {
     @Query("SELECT * FROM news")
     fun getBookmarkedNews(): LiveData<List<NewsEntity>>
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertNews(news: List<NewsEntity>)
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun saveNews(news: NewsEntity)

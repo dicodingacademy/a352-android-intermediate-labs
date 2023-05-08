528e4e3280cb8e402ab7940e1adeae34327c1c2f
Arif <faizin.arief@gmail.com>
2023-05-08 20:38:14 +0700

    Update maps to Flamingo template

--- geo-location/LatihanGoogleMaps/app/src/main/java/com/dicoding/mygooglemaps/MapsActivity.kt
+++ geo-location/LatihanGoogleMaps/app/src/main/java/com/dicoding/mygooglemaps/MapsActivity.kt
@@ -1,12 +1,10 @@
 package com.dicoding.mygooglemaps
 
-import android.Manifest
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
-import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -23,14 +21,18 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.*
-import java.io.IOException
-import java.util.*
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private val boundsBuilder = LatLngBounds.Builder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +40,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -52,9 +64,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isMapToolbarEnabled = true
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
         val dicodingSpace = LatLng(-6.8957643, 107.6338462)
         mMap.addMarker(
@@ -101,18 +113,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
                 true
             }
+
             R.id.satellite_type -> {
                 mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
                 true
             }
+
             R.id.terrain_type -> {
                 mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
                 true
             }
+
             R.id.hybrid_type -> {
                 mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
                 true
             }
+
             else -> {
                 super.onOptionsItemSelected(item)
             }
@@ -149,12 +165,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun getMyLocation() {
         if (ContextCompat.checkSelfPermission(
                 this.applicationContext,
-                Manifest.permission.ACCESS_FINE_LOCATION
+                android.Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             mMap.isMyLocationEnabled = true
         } else {
-            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
 
@@ -163,10 +179,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             val success =
                 mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
             if (!success) {
-                Log.e(TAG, "Style parsing failed.")
+                Log.e(Companion.TAG, "Style parsing failed.")
             }
         } catch (exception: Resources.NotFoundException) {
-            Log.e(TAG, "Can't find style. Error: ", exception)
+            Log.e(Companion.TAG, "Can't find style. Error: ", exception)
         }
     }
 
@@ -176,23 +192,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val longitude: Double
     )
 
-    private val boundsBuilder = LatLngBounds.Builder()
-
     private fun addManyMarker() {
         val tourismPlace = listOf(
-            TourismPlace("Floating Market Lembang", -6.8168954,107.6151046),
-            TourismPlace("The Great Asia Africa", -6.8331128,107.6048483),
-            TourismPlace("Rabbit Town", -6.8668408,107.608081),
-            TourismPlace("Alun-Alun Kota Bandung", -6.9218518,107.6025294),
+            TourismPlace("Floating Market Lembang", -6.8168954, 107.6151046),
+            TourismPlace("The Great Asia Africa", -6.8331128, 107.6048483),
+            TourismPlace("Rabbit Town", -6.8668408, 107.608081),
+            TourismPlace("Alun-Alun Kota Bandung", -6.9218518, 107.6025294),
             TourismPlace("Orchid Forest Cikole", -6.780725, 107.637409),
         )
         tourismPlace.forEach { tourism ->
             val latLng = LatLng(tourism.latitude, tourism.longitude)
-            val addressName = getAddressName(tourism.latitude, tourism.longitude)
-            mMap.addMarker(MarkerOptions().position(latLng).title(tourism.name).snippet(addressName))
+            mMap.addMarker(
+                MarkerOptions().position(latLng).title(tourism.name)
+            )
             boundsBuilder.include(latLng)
         }
-
         val bounds: LatLngBounds = boundsBuilder.build()
         mMap.animateCamera(
             CameraUpdateFactory.newLatLngBounds(
@@ -204,23 +218,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         )
     }
 
-    private fun getAddressName(lat: Double, lon: Double): String? {
-        var addressName: String? = null
-        val geocoder = Geocoder(this@MapsActivity, Locale.getDefault())
-        try {
-            val list = geocoder.getFromLocation(lat, lon, 1)
-            if (list != null && list.size != 0) {
-                addressName = list[0].getAddressLine(0)
-                Log.d(TAG, "getAddressName: $addressName")
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return addressName
-    }
-
     companion object {
         private const val TAG = "MapsActivity"
     }
-
 }

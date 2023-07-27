c2df04b6751a6247ccf5164b75478d7beb15f4e8
Arif <faizin.arief@gmail.com>
2023-07-27 21:55:17 +0700

    adjust to kts

--- firebase/LatihanFirebaseChat (fcm)/app/build.gradle.kts
+++ firebase/LatihanFirebaseChat (fcm)/app/build.gradle.kts
@@ -1,57 +1,57 @@
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
-    id 'com.google.gms.google-services'
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
-    compileSdk 33
+    compileSdk = 33
 
     defaultConfig {
-        applicationId "com.dicoding.myfirebasechat"
-        minSdk 23
-        targetSdk 33
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.dicoding.myfirebasechat"
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
 
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
-        release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        named("release") {
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
         }
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
 
     buildFeatures {
-        viewBinding true
+        viewBinding = true
     }
-    namespace 'com.dicoding.myfirebasechat'
+    namespace = "com.dicoding.myfirebasechat"
 }
 
 dependencies {
-    implementation 'androidx.core:core-ktx:1.10.1'
-    implementation 'androidx.appcompat:appcompat:1.6.1'
-    implementation 'com.google.android.material:material:1.9.0'
-    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
-    testImplementation 'junit:junit:4.13.2'
-    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
-    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation 'com.google.firebase:firebase-auth-ktx:22.1.0'
-    implementation 'com.google.android.gms:play-services-auth:20.6.0'
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.0")
+    implementation("com.google.android.gms:play-services-auth:20.6.0")
 
-    implementation 'com.google.firebase:firebase-database-ktx:20.2.2'
-    implementation 'com.firebaseui:firebase-ui-database:8.0.0'
-    implementation 'com.github.bumptech.glide:glide:4.15.1'
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
+    implementation("com.firebaseui:firebase-ui-database:8.0.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
 
-    implementation 'com.google.firebase:firebase-messaging-ktx:23.2.0'
+    implementation("com.google.firebase:firebase-messaging-ktx:23.2.0")
 }

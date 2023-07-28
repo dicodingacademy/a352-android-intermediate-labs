cacf0e55419244838ea6b4cc1d757965b62e24a4
Arif <faizin.arief@gmail.com>
2023-07-28 16:24:47 +0700

    Remake starter project
    - Update KTS
    - Update AGP 8
    - Update Material 3
    - Update KTS
    - Login session flow
    - Add repository and injection
    - Change to byViewModels and variable name
    - Remove SignUpViewModel
    - Singleton in ViewModelFactory
    - Add tools: alpha
    - Change passwordToggleEnabled to app:endIconMode="password_toggle"
    - Color name navi to navy
    - Remove unused guildeline and copyright

---
+++ animation/LatihanPropertyAnimationStarter/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/UserRepository.kt
@@ -0,0 +1,33 @@
+package com.dicoding.picodiploma.loginwithanimation.data
+
+import com.dicoding.picodiploma.loginwithanimation.data.pref.UserModel
+import com.dicoding.picodiploma.loginwithanimation.data.pref.UserPreference
+import kotlinx.coroutines.flow.Flow
+
+class UserRepository private constructor(
+    private val userPreference: UserPreference
+) {
+
+    suspend fun saveSession(user: UserModel) {
+        userPreference.saveSession(user)
+    }
+
+    fun getSession(): Flow<UserModel> {
+        return userPreference.getSession()
+    }
+
+    suspend fun logout() {
+        userPreference.logout()
+    }
+
+    companion object {
+        @Volatile
+        private var instance: UserRepository? = null
+        fun getInstance(
+            userPreference: UserPreference
+        ): UserRepository =
+            instance ?: synchronized(this) {
+                instance ?: UserRepository(userPreference)
+            }.also { instance = it }
+    }
+}

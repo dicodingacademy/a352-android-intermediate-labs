cb16cc617f2b604a76745ad9ad0c8519b3e54dc4
Ahmad Arif Faizin <faizin.arief@gmail.com>
2023-09-05 09:26:50 +0700

    Change folder name from LatihanCameraXStarter to LatihanCameraStarter
    - KTS
    - Change button position
    - Change placeholder position
    - Add ProgressBar
    - Remove Utils
    - Use Material 3
    - Upgrade to AGP 8

---
+++ media/LatihanCameraStarter/app/src/main/java/com/dicoding/picodiploma/mycamera/CameraActivity.kt
@@ -0,0 +1,50 @@
+package com.dicoding.picodiploma.mycamera
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.picodiploma.mycamera.databinding.ActivityCameraBinding
+
+class CameraActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCameraBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.switchCamera.setOnClickListener {
+            startCamera()
+        }
+        binding.captureImage.setOnClickListener { takePhoto() }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        startCamera()
+    }
+
+    private fun startCamera() {
+        // showCamera
+    }
+
+    private fun takePhoto() {
+        // takePhoto
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}

13a1e9e13999e0de0f5b3e521b22a5479ceaa38a
IanDs <ian.indratama@gmail.com>
2023-11-16 09:50:39 +0700

    LatihanWidgets - Updated AGP 8.1.3 and Migrate to Kotlin DSL

--- advanced-ui/LatihanWidgets/settings.gradle.kts
+++ advanced-ui/LatihanWidgets/settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
@@ -12,5 +12,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
 rootProject.name = "MyWidgets"
-include ':app'
+include(":app")

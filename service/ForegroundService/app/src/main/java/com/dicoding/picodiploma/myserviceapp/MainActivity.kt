af1a1e5be87ebd4d85ff155809c5d75fbc6274e6
ianindratama <ian.indratama@gmail.com>
2023-12-04 14:49:11 +0700

    LatihanForegroundService - Added Foreground Service implementation for API 31, 33, and 34 & Migrate to Material3

--- service/ForegroundService/app/src/main/java/com/dicoding/picodiploma/myserviceapp/MainActivity.kt
+++ service/ForegroundService/app/src/main/java/com/dicoding/picodiploma/myserviceapp/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.dicoding.picodiploma.myserviceapp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.dicoding.picodiploma.myserviceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        RequestPermission()
+    ) { isGranted: Boolean? ->
+        if (!isGranted!!)
+            Toast.makeText(this,
+            "Unable to display Foreground service notification due to permission decline",
+            Toast.LENGTH_LONG)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +37,15 @@ class MainActivity : AppCompatActivity() {
             stopService(serviceIntent)
         }
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+
         val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
         binding.btnStartForegroundService.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= 26) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 startForegroundService(foregroundServiceIntent)
             } else {
                 startService(foregroundServiceIntent)
@@ -36,4 +56,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
 }
